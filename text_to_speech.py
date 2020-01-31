d68a3c9a1dfa821bd3e72441dbc840691f6741e0
Zach <zscientist100@hotmail.com>
2020-01-31 11:30:11 -0500

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,9 +1,11 @@
+##Modules to import##
 import os, time
 
+##Use and call to modules##
 def robot(text):
     os.system("espeak ' " + text + " ' ")
 
-
+##Continues The Program By Requestinng a new input infinitly##
 while True:
     talk = input('>>> ')
     robot(' ' + talk)
